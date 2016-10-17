7c44774384dd283c34a36397b1acc9a388ae73b9
Aidan Feldman <alf239@cornell.edu>
2016-10-17 15:56:52 +0000

    set up a test database for each test

--- squawker/server.py
+++ squawker/server.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, g
+import sqlite3
 
 
+# -- leave these lines intact --
 app = Flask(__name__)
+
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        db_name = app.config.get('DATABASE', 'squawker.db')
+        g.sqlite_db = sqlite3.connect(db_name)
+
+    return g.sqlite_db
+# ------------------------------
 
 
 @app.route('/')
 def hello():
+    db = get_db()
+    c = db.cursor()
+    # ...
     return "Hello World!"
 
 
--- tests/test_squawker.py
+++ tests/test_squawker.py
@@ -1,12 +1,26 @@
+import os
 import pytest
 from squawker import server
+import tempfile
 
 
 @pytest.fixture()
-def app():
-    server.app.config['TESTING'] = True
+def db_fd():
+    print("creating database")
+    db_fd, server.app.config['DATABASE'] = tempfile.mkstemp()
+    return db_fd
+
+@pytest.fixture()
+def app(db_fd):
     return server.app.test_client()
 
+
+def setup_method(self, method):
+    server.app.config['TESTING'] = True
+
+def teardown_method(self, method):
+    os.close(self.db_fd)
+    os.unlink(server.app.config['DATABASE'])
 
 def test_response_code(app):
     response = app.get('/')
