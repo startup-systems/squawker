55e2231f1a4d4348eaca7304a3a64fa9ca19c38f
awsmdavid <awsmdavid@gmail.com>
2016-10-18 12:51:51 -0400

    added pagination, need to clean up code comments and run pytests

--- squawker/server.py
+++ squawker/server.py
@@ -36,98 +36,113 @@ def close_connection(exception):
         db.close()
 # ------------------------------
 
-# class Pagination(object):
 
-#     def __init__(self, page, per_page, total_count):
-#         self.page = page
-#         self.per_page = per_page
-#         self.total_count = total_count
-
-#     @property
-#     def pages(self):
-#         return int(ceil(self.total_count / float(self.per_page)))
-
-#     @property
-#     def has_prev(self):
-#         return self.page > 1
-
-#     @property
-#     def has_next(self):
-#         return self.page < self.pages
-
-#     def iter_pages(self, left_edge=2, left_current=2,
-#                    right_current=5, right_edge=2):
-#         last = 0
-#         for num in xrange(1, self.pages + 1):
-#             if num <= left_edge or \
-#                (num > self.page - left_current - 1 and \
-#                 num < self.page + right_current) or \
-#                num > self.pages - right_edge:
-#                 if last + 1 != num:
-#                     yield None
-#                 yield num
-#                 last = num
+PER_PAGE = 20
 
 
-# PER_PAGE = 20
+class Pagination(object):
 
-# @app.route('/squawks/', defaults={'page': 1})
-# @app.route('/squawks/page/<int:page>')
-# def show_squawks(page):
-#     count = count_all_squawks()
-#     squawks = get_squawks_for_page(page, PER_PAGE, count)
-#     if not squawks and page != 1:
-#         abort(404)
-#     pagination = Pagination(page, PER_PAGE, count)
-#     return render_template('index.html',
-#         pagination=pagination,
-#         squawks=squawks
-#     )
+    def __init__(self, page, per_page, total_count):
+        self.page = page
+        self.per_page = per_page
+        self.total_count = total_count
 
-# def count_all_squawks():
-#     # create db connection
-#     conn = get_db()
-#     # create cursor object with squawk query
-#     cursor_object = conn.execute('SELECT ID, squawk_text from squawks order by id desc')
-#     # iterate over all squawks and store
-#     squawks = cursor_object.fetchall()
-#     return len(squawks)
+    @property
+    def pages(self):
+        return int(ceil(self.total_count / float(self.per_page)))
 
-# def get_squawks_for_page(page, PER_PAGE, count):
-#     # create db connection
-#     conn = get_db()
-#     # create cursor object with squawk query
-#     cursor_object = conn.execute('SELECT ID, squawk_text from squawks order by id desc')
-#     # iterate over all squawks and store
-#     squawks = cursor_object.fetchall()
-#     if (page==1):
-#         i = 0
-#         j = 20
-#     else:
-#         i = page*count+1
-#         j = (2*page)*count
+    @property
+    def has_prev(self):
+        return self.page > 1
 
-#     squawks = squawks[i:j]
-#     return squawks
+    @property
+    def has_next(self):
+        return self.page < self.pages
+
+    def iter_pages(self, left_edge=2, left_current=2,
+                   right_current=5, right_edge=2):
+        last = 0
+        for num in xrange(1, self.pages + 1):
+            if num <= left_edge or \
+               (num > self.page - left_current - 1 and \
+                num < self.page + right_current) or \
+               num > self.pages - right_edge:
+                if last + 1 != num:
+                    yield None
+                yield num
+                last = num
 
 
-# def url_for_other_page(page):
-#     args = request.view_args.copy()
-#     args['page'] = page
-#     return url_for(request.endpoint, **args)
-# app.jinja_env.globals['url_for_other_page'] = url_for_other_page
+@app.route('/squawks/', defaults={'page': 1})
+@app.route('/squawks/page/<int:page>')
+def show_squawks(page):
+    count = count_all_squawks()
+    squawks = get_squawks_for_page(page, PER_PAGE, count)
+    if not squawks and page != 1:
+        abort(404)
+    pagination = Pagination(page, PER_PAGE, count)
+    return render_template('index.html',
+        pagination=pagination,
+        squawks=squawks
+    )
 
-
-# root route
-@app.route('/')
-def root():
+def count_all_squawks():
     # create db connection
     conn = get_db()
     # create cursor object with squawk query
     cursor_object = conn.execute('SELECT ID, squawk_text from squawks order by id desc')
     # iterate over all squawks and store
     squawks = cursor_object.fetchall()
-    return render_template('index.html', squawks=squawks)
+    return len(squawks)
+
+def get_squawks_for_page(page, PER_PAGE, count):
+    # create db connection
+    conn = get_db()
+    # create cursor object with squawk query
+    cursor_object = conn.execute('SELECT ID, squawk_text from squawks order by id desc')
+    # iterate over all squawks and store
+    squawks = cursor_object.fetchall()
+    if (page==1):
+        i = 0
+        j = 20
+    else:
+        i = ((page - 1) * PER_PAGE)
+        j = page * PER_PAGE
+
+    squawks = squawks[i:j]
+    return squawks
+
+
+def url_for_other_page(page):
+    args = request.view_args.copy()
+    args['page'] = page
+    return url_for(request.endpoint, **args)
+app.jinja_env.globals['url_for_other_page'] = url_for_other_page
+
+
+@app.route('/', defaults={'page': 1})
+@app.route('/page/<int:page>')
+def root(page, defaults=1):
+    count = count_all_squawks()
+    squawks = get_squawks_for_page(page, PER_PAGE, count)
+    # if not squawks and page != 1:
+    #     abort(404)
+    pagination = Pagination(page, PER_PAGE, count)
+    return render_template('index.html',
+        pagination=pagination,
+        squawks=squawks
+    )
+
+# root route
+# @app.route('/')
+# def root():
+#     # create db connection
+#     conn = get_db()
+#     # create cursor object with squawk query
+#     cursor_object = conn.execute('SELECT ID, squawk_text from squawks order by id desc')
+#     # iterate over all squawks and store
+#     squawks = cursor_object.fetchall()
+#     return render_template('index.html', squawks=squawks)
 
 
 # add a squawk via post request
