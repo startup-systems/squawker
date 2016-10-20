d4878d933d1a0305e547265c627d5e27857362b8
rwg58 <rwg58@mail.missouri.edu>
2016-10-19 22:04:52 -0400

    first try

--- squawker/schema.sql
+++ squawker/schema.sql
@@ -1,3 +1,3 @@
 -- TODO change this
 DROP TABLE IF EXISTS mytable;
-CREATE TABLE mytable (id integer);
+CREATE TABLE mytable (id INTEGER PRIMARY KEY NOT NULL, content varchar(140), timestamp datetime DEFAULT CURRENT_TIMESTAMP);
