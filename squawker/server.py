9f7fe30c4dd00b9685759d2b5b5a3280c75c2b62
mw866 <mw866@cornell.edu>
2016-10-20 23:31:07 -0400

    Pending Server-side validation

--- squawker/server.py
+++ squawker/server.py
@@ -1,9 +1,9 @@
-import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
 # -- leave these lines intact --
 app = Flask(__name__)
+app.secret_key = 'b73730fa69991bf9e4702e49cec96c63' # Added by mw866
 
 
 def get_db():
@@ -41,18 +41,16 @@ def close_connection(exception):
 def show_entries():
     db = get_db()
     # TODO change this
-    #return "Hello World!"
     cur = db.execute('select title, text from entries order by id desc')
-    entries = cur.fetchall()
+    #entries = cur.fetchall()
+    entries = [{"title" : row[0], "text" : row[1]} for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
 
 
 @app.route('/add', methods=['POST'])
 def add_entry():
-    #if not session.get('logged_in'):
-    #        abort(401)
     db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',[request.form['title'], request.form['text']])
+    db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
     db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
