297609983302dfc05d91d4c45e7d52584e72fc3a
Aidan Feldman <alf239@cornell.edu>
2016-10-22 04:08:26 +0000

    add tests around form being rendered, and creating a squawk

--- tests/test_squawker.py
+++ tests/test_squawker.py
@@ -1,5 +1,7 @@
+from bs4 import BeautifulSoup
 import os
 import pytest
+from splinter import Browser
 from squawker import server
 import tempfile
 
@@ -7,7 +9,6 @@ import tempfile
 @pytest.fixture()
 def db_fd():
     server.app.config['TESTING'] = True
-    print("creating database")
     db_fd, server.app.config['DATABASE'] = tempfile.mkstemp()
     return db_fd
 
@@ -27,10 +28,34 @@ def db_client(db_fd, request):
 
 
 @pytest.fixture()
-def app(db_client):
+def test_app(db_client):
     return server.app.test_client()
 
 
-def test_response_code(app):
-    response = app.get('/')
+def test_response_code(test_app):
+    response = test_app.get('/')
     assert response.status_code == 200
+
+
+@pytest.mark.score(5)
+def test_form_present(test_app):
+    response = test_app.get('/')
+    soup = BeautifulSoup(response.data, 'html.parser')
+    form = soup.find('form')
+    assert form is not None
+
+
+@pytest.mark.score(30)
+def test_create_squawk(db_client):
+    browser = Browser('flask', app=server.app)
+    url = '/'
+    browser.visit(url)
+
+    TEXT = "splinter - python acceptance testing for web applications"
+    input_el = browser.find_by_css('input[type="text"],textarea').first
+    assert input_el is not None
+    input_el.fill(TEXT)
+    button = browser.find_by_css('input[type="submit"]').first
+    button.click()
+
+    assert browser.is_text_present(TEXT)
