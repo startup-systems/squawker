ce911abf9c2c579a2e5e4b4cbd7310f573159bce
Renee <rle87@cornell.edu>
2016-10-22 23:07:16 +0000

    Updated server code to include to post a new sqawk and get all squawks. Included pagination

--- squawker/server.py
+++ squawker/server.py
@@ -1,6 +1,6 @@
-from flask import Flask, g
+from flask import Flask, g, render_template, request, redirect, url_for, abort
 import sqlite3
-
+from math import ceil
 
 # -- leave these lines intact --
 app = Flask(__name__)
@@ -37,11 +37,79 @@ def close_connection(exception):
 # ------------------------------
 
 
-@app.route('/')
-def root():
+class Pagination(object):
+    # Reference: http://flask.pocoo.org/snippets/44/
+
+    def __init__(self, page, per_page, total_count):
+        self.page = page
+        self.per_page = per_page
+        self.total_count = total_count
+
+    @property
+    def pages(self):
+        return int(ceil(self.total_count / float(self.per_page)))
+
+    @property
+    def has_prev(self):
+        return self.page > 1
+
+    @property
+    def has_next(self):
+        return self.page < self.pages
+
+    def iter_pages(self, left_edge=2, left_current=2,
+                   right_current=5, right_edge=2):
+        last = 0
+        for num in range(1, self.pages + 1):
+            if num <= left_edge or \
+               (num > self.page - left_current - 1 and
+                num < self.page + right_current) or \
+               num > self.pages - right_edge:
+                if last + 1 != num:
+                    yield None
+                yield num
+                last = num
+
+PER_PAGE = 20
+
+
+def get_squawks_for_page(squawks, page):
+    # return the squawks for the current page
+    start_index = ((page - 1) * PER_PAGE)
+    end_index = min(start_index + PER_PAGE, len(squawks))
+    squawks = squawks[start_index:end_index]
+    return squawks
+
+
+def url_for_other_page(page):
+    args = request.view_args.copy()
+    args['page'] = page
+    return url_for(request.endpoint, **args)
+app.jinja_env.globals['url_for_other_page'] = url_for_other_page
+
+
+@app.route('/', methods=["GET", "POST"], defaults={'page': 1})
+@app.route('/page/<int:page>')
+def root(page):
+    error = ""
+    status = 200
     conn = get_db()
-    # TODO change this
-    return "Hello World!"
+    if(request.method == "POST"):
+        post_text = request.form['post_text']
+        if len(post_text) > 140:
+            status = 400
+            error = "Error: Your post is too long! Keep to 140 characters, please."
+        else:
+            c = conn.execute('INSERT INTO squawks (text) VALUES (?)', [post_text])
+            conn.commit()
+    c = conn.execute('SELECT * FROM squawks ORDER BY id desc')
+    squawks = c.fetchall()
+    count = len(squawks)
+    squawks = get_squawks_for_page(squawks, page)
+    if not squawks and page != 1:
+        abort(404)
+    pagination = Pagination(page, PER_PAGE, count)
+    return render_template('index.html', pagination=pagination, squawks=squawks, error=error), status
 
 
 if __name__ == '__main__':
