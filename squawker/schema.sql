ea12e156d162ea6f78cab80c58f60554dd765bb9
travis holt <trvslhlt@gmail.com>
2016-10-23 22:39:32 +0000

    Creates squawk on POST to /squawk

--- squawker/schema.sql
+++ squawker/schema.sql
@@ -1,3 +1,8 @@
 -- TODO change this
-DROP TABLE IF EXISTS mytable;
-CREATE TABLE mytable (id integer);
+DROP TABLE IF EXISTS squawks;
+CREATE TABLE squawks (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	time REAL,
+	username TEXT,
+	text TEXT
+);
