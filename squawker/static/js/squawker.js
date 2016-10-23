b74acaf5ed838044171391a1068ca68d28e34e88
travis holt <trvslhlt@gmail.com>
2016-10-23 21:15:11 +0000

    Printing squawk body onclick
    - not POSTing

--- squawker/static/js/squawker.js
+++ squawker/static/js/squawker.js
@@ -1,4 +1,17 @@
 
-function clickedSquawk() {
-	console.log("squawk");
+function squawk() {
+	$(function () {
+		var squawkComposer = $("#squawkComposer");
+		var text = squawkComposer.val();
+		var date = new Date();
+		var time = date.getTime();
+		var username = "trvslhlt";
+		var body = {
+			"text": text,
+			"time": time,
+			"username": username
+		};
+		console.log(JSON.stringify(body));
+	});
+	
 }
