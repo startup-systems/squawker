f76a5e8c266c824d046c5f636b58d806d3b8c776
Shanshan Zhang <sz438@cornell.com>
2016-10-24 05:39:15 +0000

    first try

--- squawker/schema.sql
+++ squawker/schema.sql
@@ -1,3 +1,7 @@
 -- TODO change this
-DROP TABLE IF EXISTS mytable;
-CREATE TABLE mytable (id integer);
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts (id INTEGER PRIMARY KEY AUTOINCREMENT,post string);
+INSERT INTO posts(post) VALUES ("I am very hungry!");
+INSERT INTO posts(post) VALUES ("I do not like doing homework!");
+INSERT INTO posts(post) VALUES ("I want to buy a new dress!");
+
