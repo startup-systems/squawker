3e1d97277260a1044bedada29db08e1969ffca63
interactivetech <interactivetech1@gmail.com>
2016-10-24 15:20:41 -0400

    Fixed another file to have pep8 format

--- dbTest.py
+++ dbTest.py
@@ -1,15 +1,14 @@
 import sqlite3
 
 conn = sqlite3.connect('squaker.db')
-print "Opened database successfully";
+print "Opened database successfully"
 
 cur = conn.cursor()
 conn.execute('CREATE TABLE IF NOT EXISTS tweets2 (tweet TEXT)')
-print "Table created successfully";
+print "Table created successfully"
 
 
-
-cur.execute("INSERT INTO tweets2 (tweet) VALUES (?)",("Hello Squaker!",) )
+cur.execute("INSERT INTO tweets2 (tweet) VALUES (?)", ("Hello Squaker!",))
 
 
 conn.commit()
@@ -18,5 +17,5 @@ msg = "Record successfully added"
 
 cur.execute("select * from tweets2")
 
-print cur.fetchall();
+print cur.fetchall()
 conn.close()
