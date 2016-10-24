80fa2af636b4065aff754d5280039646e190f1be
Ubuntu <jillian.sue3@gmail.com>
2016-10-24 21:06:44 +0000

    hw4

--- squawker/schema.sql
+++ squawker/schema.sql
@@ -2,5 +2,5 @@ DROP TABLE IF EXISTS squawks;
 CREATE TABLE squawks(
     id integer PRIMARY KEY,
     squawk varchar(140),
-    timestamp datetime DEFAULT CURRENT_TIMESTAMP,
+    timestamp datetime DEFAULT CURRENT_TIMESTAMP
 );
