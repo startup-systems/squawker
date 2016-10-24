34ce5fce213ede2b92ac00c04a7230bcba20d7cc
trishalaneeraj <trishalaneeraj@gmail.com>
2016-10-24 20:27:02 +0000

    final check

--- squawker/schema.sql
+++ squawker/schema.sql
@@ -1,8 +1,8 @@
 
 -- TODO change this
-DROP TABLE IF EXISTS squawker
+DROP TABLE IF EXISTS squawker;
 CREATE TABLE squawker(
     id integer primary key,
     squawk varchar(140),
     time datetime CURRENT_TIMESTAMP
-)
+);
