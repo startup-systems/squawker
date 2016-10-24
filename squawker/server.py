c6824922d039f65e0d4376b69b51fc5c281fb133
trishalaneeraj <trishalaneeraj@gmail.com>
2016-10-24 17:15:19 +0000

    Submission files

--- squawker/server.py
+++ squawker/server.py
@@ -1,5 +1,7 @@
 from flask import Flask, g
 import sqlite3
+from flask import render_template
+from flask import request, session, url_for, redirect, abort, g, flash, _app_ctx_stack
 
 
 # -- leave these lines intact --
@@ -13,14 +15,12 @@ def get_db():
 
     return g.sqlite_db
 
-
 def init_db():
     with app.app_context():
         db = get_db()
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
-
 
 @app.cli.command('initdb')
 def initdb_command():
@@ -34,15 +34,123 @@ def close_connection(exception):
     db = getattr(g, 'sqlite_db', None)
     if db is not None:
         db.close()
+
+
 # ------------------------------
 
 
-@app.route('/')
+# @app.route('/')
+# def root():
+#     conn = get_db()
+#     #TODO change this
+
+
+#     return "Hello, post a new squawk!"
+
+
+@app.route('/', methods=["POST", "GET"])
 def root():
+
     conn = get_db()
-    # TODO change this
-    return "Hello World!"
+    c = conn.cursor()
+
+    if request.method == 'POST':
+        new_msg = request.form.get('msg')
+        if len(new_msg) > 140:
+            return "Length error! Reduce length of squawk to 140 characters or less.", 400
+        else:
+            query = "INSERT INTO squawker (squawk) VALUES (?)"
+            c.execute(query, [new_msg])
+            conn.commit()
+
+    # displaying all the posts in order
+    selectquery = "SELECT squawk FROM squawker order by id DESC"
+    c.execute(selectquery)
+    all = c.fetchall()
+    c.close()
+    return render_template("form.html", squawks=all)
 
 
-if __name__ == '__main__':
-    app.run()
+# @app.route('/') #set default?
+# @app.route('/', method=["GET","POST"])
+# @app.route('/page/<int:page>')
+#
+#
+# def root(page):
+#     return render_template('form.html')
+
+
+# @app.route('/hello')
+# def hello():
+#     return 'Hello, World'
+
+
+# @app.route('/user/<username>')
+# def show_user_profile(username):
+#     # show the user profile for that user
+#     return 'User %s' % username
+
+# @app.route('/post/<int:post_id>')
+# def show_post(post_id):
+#     # show the post with the given id, the id is an integer
+#     return 'Post %d' % post_id
+
+
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         do_the_login()
+#     else:
+#         show_the_login_form()
+
+
+# #@app.route('/hello/')
+# @app.route('/hello/<name>')
+
+# def hello(name=None):
+#     return render_template('hello.html', name=name)
+
+
+# with app.test_request_context('/hello', method='POST'):
+#     # now you can do something with the request until the
+#     # end of the with block, such as basic assertions:
+#     assert request.path == '/hello'
+#     assert request.method == 'POST'
+
+
+# # To record whatever is submitted via the form.
+# @app.route('/add_message', methods=['POST'])
+# def add_message():
+#     """Registers a new message for the user."""
+#     # if 'user_id' not in session:
+#     #     abort(401)
+#     if request.form['text']:
+#         db = get_db()
+#         db.execute('''insert into message (text, pub_date) values (?, ?)''', (request.form['text'], int(time.time())))
+#         db.commit()
+#         flash('Your message was recorded')
+#     return redirect(url_for('timeline'))
+
+
+# # To display everything that has been submitted by the form.
+# @app.route('/public')
+# def public_timeline():
+#     """Displays the latest messages of all users."""
+#     return render_template('timeline.html', messages=query_db('''
+#         select message.* from message where order by message.pub_date desc limit ?''', [PER_PAGE]))
+
+# #Is this necessary? The previous function effectively does the work.
+# @app.route('/')
+# def timeline():
+#     """Shows a users timeline or if no user is logged in it will
+#     redirect to the public timeline.  This timeline shows the user's
+#     messages as well as all the messages of followed users.
+#     """
+#     return render_template('timeline.html', messages=query_db('''select message.* from message
+#         where
+#         order by message.pub_date desc limit ?''',
+#         [PER_PAGE]))
+
+# if __name__ == '__main__':
+#     app.run()
