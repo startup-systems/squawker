f71009b0d17db6d38a7fa9082d3a49f9e62b8f10
Bruce-Yu-Xi <xy333@cornell.edu>
2016-10-24 18:00:04 -0400

    Update schema.sql

--- squawker/schema.sql
+++ squawker/schema.sql
@@ -2,6 +2,6 @@
 DROP TABLE IF EXISTS mytable;
 CREATE TABLE squawks (
 	id integer primary key autoincrement,
-	posts varchar(140),
-	timestamp datetime default CURRENT_TIMESTAMP
+	squawk varchar(140),
+	time_stamp datetime default CURRENT_TIMESTAMP
 );
