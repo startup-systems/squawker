0648e1a33c114be0bbd22e7bf3e8979c4e98587a
Gautam Chheda <gc463@cornell.edu>
2016-10-24 19:47:47 -0400

    Update server.py

--- squawker/server.py
+++ squawker/server.py
@@ -7,33 +7,33 @@ app = Flask(__name__)
 
 
 def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        db_name = app.config.get('DATABASE', 'squawker.db')
-        g.sqlite_db = sqlite3.connect(db_name)
+	if not hasattr(g, 'sqlite_db'):
+        	db_name = app.config.get('DATABASE', 'squawker.db')
+        	g.sqlite_db = sqlite3.connect(db_name)
 
-    return g.sqlite_db
+    	return g.sqlite_db
 
 
 def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+	with app.app_context():
+        	db = get_db()
+        	with app.open_resource('schema.sql', mode='r') as f:
+            		db.cursor().executescript(f.read())
+        	db.commit()
 
 
 @app.cli.command('initdb')
 def initdb_command():
-    """Creates the database tables."""
-    init_db()
-    print('Initialized the database.')
+	"""Creates the database tables."""
+    	init_db()
+    	print('Initialized the database.')
 
 
 @app.teardown_appcontext
 def close_connection(exception):
-    db = getattr(g, 'sqlite_db', None)
-    if db is not None:
-        db.close()
+    	db = getattr(g, 'sqlite_db', None)
+    	if db is not None:
+        	db.close()
 # ------------------------------
 
 
@@ -42,16 +42,16 @@ def root():
 	conn = get_db()
 	c = conn.cursor()
     	if request.method == "POST":
-	post = request.form.get('squawker_post')
-        	if len(post) > 140:
-			abort(400)
-		elif len(post)>0 and len(post)<=140:
-			c.execute("INSERT INTO mytable (squawk) VALUES(?);", [post])
-			conn.commit()
-    	c.execute("SELECT * FROM mytable ORDER BY id DESC")
-    	squawks = c.fetchall()
-    	return render_template("index.html", squawks=squawks)
+		post = request.form.get('squawker_post')
+        		if len(post) > 140:
+				abort(400)
+			elif len(post)>0 and len(post)<=140:
+				c.execute("INSERT INTO mytable (squawk) VALUES(?);", [post])
+				conn.commit()
+    		c.execute("SELECT * FROM mytable ORDER BY id DESC")
+    		squawker = c.fetchall()
+    		return render_template("index.html", squawks=squawker)
 
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
