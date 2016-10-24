847e5a7ffa52823b5bded863c32e6731f3ba03f3
Chen Zhang <recursion915@users.noreply.github.com>
2016-10-24 17:33:36 -0400

    Update schema.sql

--- squawker/schema.sql
+++ squawker/schema.sql
@@ -3,5 +3,5 @@ DROP TABLE IF EXISTS squawks;
 CREATE TABLE squawks (
 	id integer primary key autoincrement,
 	posts varchar(140),
-	time datetime default CURRENT_TIMESTAMP
+	timestamp datetime default CURRENT_TIMESTAMP
 );
