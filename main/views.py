fcaa3a3b290390c5e7ac05d46f084812ccfa3887
vagrant <abhinandam@gmail.com>
2016-11-01 23:35:12 +0000

    update indentation

--- main/views.py
+++ main/views.py
@@ -5,13 +5,13 @@ from .models import Squawk
 
 
 def root(request):
-  if (request.method == "POST"):
-    inputMessage = request.POST['message']
+    if (request.method == "POST"):
+        inputMessage = request.POST['message']
     if (len(inputMessage) <= 140):
-      squawk = Squawk(message=inputMessage)
-      squawk.save()
-  else:
-    return HttpResponseBadRequest("Error: Message Exceeded Max Length of 140")
+        squawk = Squawk(message=inputMessage)
+        squawk.save()
+    else:
+        return HttpResponseBadRequest("Error: Message Exceeded Max Length of 140")
 
-  squawkers = Squawk.objects.order_by('-created')
-  return render(request, "index.html", {"squawks":squawkers})
+    squawkers = Squawk.objects.order_by('-created')
+    return render(request, "index.html", {"squawks":squawkers})
