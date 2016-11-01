617b7495099e09958fef2efe2056ab6128a216d5
vagrant <abhinandam@gmail.com>
2016-11-01 03:00:59 +0000

    push to heroku

--- squawker/settings.py
+++ squawker/settings.py
@@ -1,3 +1,5 @@
+# add this near the top
+import dj_database_url
 """
 Django settings for squawker project.
 
@@ -74,14 +76,10 @@ WSGI_APPLICATION = 'squawker.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
+# replace the DATABASES config
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    "default": dj_database_url.config(default='sqlite:///db.sqlite3'),
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
